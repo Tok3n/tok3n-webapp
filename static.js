9296a59956de5153a8f2ac71c351291db3c59852
Victor Gonzalez <victor@tok3n.com>
2013-09-13 07:59:22 -0500

    Add node static support

---
+++ static.js
@@ -0,0 +1,10 @@
+var static = require('node-static');
+
+var fileServer = new static.Server('./dist');
+var port = process.env.PORT || 5000;
+
+require('http').createServer(function (request, response) {
+    request.addListener('end', function () {
+        fileServer.serve(request, response);
+    }).resume();
+}).listen(port);
